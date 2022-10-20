0ea5e0ca5d16c77caf2761383665ea2fdf0a1f83
Decodon <20095352@mail.wit.ie>
2022-10-20 21:19:11 +0100

    Week 4 - Exercise Solution. Adding start to the controller

--- src/main/kotlin/org/setu/placemark/console/controllers/PlacemarkController.kt
+++ src/main/kotlin/org/setu/placemark/console/controllers/PlacemarkController.kt
@@ -16,6 +16,25 @@ class PlacemarkController {
         println("Placemark Kotlin App Version 1.0")
     }
 
+    fun start() {
+        var input: Int
+
+        do {
+            input = menu()
+            when (input) {
+                1 -> add()
+                2 -> update()
+                3 -> list()
+                4 -> search()
+                -99 -> dummyData()
+                -1 -> println("Exiting App")
+                else -> println("Invalid Option")
+            }
+            println()
+        } while (input != -1)
+        logger.info { "Shutting Down Placemark Console App" }
+    }
+
     fun menu() :Int { return placemarkView.menu() }
 
     fun add(){
